7df1d2da6d32d867dd6ff0722e6c7e3d154ed055
Alejandro Redondo Villa <alexredondovilla@gmail.com>
2022-06-29 16:12:09 +0200

    pasar el setState en las props a un componente hijo

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -5,29 +5,28 @@ import { AddCategory } from "./components/AddCategory";
  * Componente principal de la aplicacion
  */
 export const GifApp = () => {
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-  console.log('>> categories', categories);
+  console.log(">> categories", categories);
 
   const handleAddCategory = () => {
-    setCategories([...categories, 'Iron Man'])
-  }
+    setCategories([...categories, "Iron Man"]);
+  };
+  
   return (
     <>
       {/* TITULO */}
       <h1>GifApp</h1>
 
       {/* INPUT */}
-      <AddCategory />
+      <AddCategory setCategories={setCategories} />
       {/* LISTADO */}
       <button onClick={handleAddCategory}>Agregar</button>
       <ol>
-        {
-          categories.map(category => {
-            return <li key={category}>{category}</li>
-          })
-        }
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
       </ol>
-    </>    
+    </>
   );
 };
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
-export const AddCategory = () => {
-  const [inputValue, setInputValue] = useState("Iron Man");
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
 
   const onChangeInput = (event) => {
     // console.log('>> event', event.target.value);
@@ -10,9 +10,16 @@ export const AddCategory = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault(); // EVITA QUE SE ACTUALICE LA PAGINA CUANDO HAGAMOS submit
-    console.log('>> event submit', event);
-    console.log('inputValue', inputValue);
-  }
+    // console.log(">> event submit", event);
+    // console.log("inputValue", inputValue);
+    setCategories((categories) => [inputValue, ...categories]);
+    if (inputValue.trim().length <= 1) return;
+    // SE MANDA EL setCategories MEDIAENTE LAS PROPS, PERO COMO NO TENEMOS LAS categories, USAMOS
+    // EL CALLBACK PARA MODIFICAR EL ARRAY DE categories, USANDO EL CALLBACK ACCEDEMOS A LA INFORMACION
+    // DE categories.
+  };
+
+  
   return (
     <form onSubmit={handleSubmit}>
       <input
