0cc24f9fcb7a93d0fc50710bf4e7e7f6d0d3a7be
Alejandro Redondo Villa <alexredondovilla@gmail.com>
2022-06-29 14:16:58 +0200

    uso basico de useState

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+
 export const GifApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+
+  console.log('>> categories', categories);
+
+  const handleAddCategory = () => {
+    setCategories([...categories, 'Iron Man'])
+  }
   return (
-    <div>GifApp</div>
-    
+    <>
+      {/* TITULO */}
+      <h1>GifApp</h1>
+
+      {/* INPUT */}
+
+      {/* LISTADO */}
+      <button onClick={handleAddCategory}>Agregar</button>
+      <ol>
+        {
+          categories.map(category => {
+            return <li key={category}>{category}</li>
+          })
+        }
+      </ol>
+    </>    
   );
 };
