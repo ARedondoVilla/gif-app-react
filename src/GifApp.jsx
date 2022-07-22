1b0590f6126753746addc96681e79356220b9cf2
Alejandro Redondo Villa <alexredondovilla@gmail.com>
2022-07-23 01:08:39 +0200

    apliacion de archivo de barril para agrupar todas las importaciones existentes en una carpeta

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+// import { AddCategory } from "./components/AddCategory";
+// import { GifGrid } from "./components/GifGrid";
+// MEDIANTE EL ARCHIVO DE BARRIL SE IMPORTA EN UNA SOLA LINEA TODOS LOS COMPONENTES QUE PERTENECEN A LA MISMA CARPETA
+// ESTE ARCHIVO AL LLAMARSE index.js PERMITE QUE EN EL PATH SOLO SE NAVEGUE HASTA LA CARPETA, NO HACE FALTA QUE INDIQUEMOS
+// EL COMPONENTE, YA QUE POR DEFECTO EL ARCHIVO AL QUE NAVEGA ES AL index.js SI ESTE EXISTE
+import {AddCategory, GifGrid} from './components'
 
 /**
  * Componente principal de la aplicacion
