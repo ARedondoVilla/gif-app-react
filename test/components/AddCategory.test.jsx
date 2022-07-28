e348b5a8cb9b2d32c9eea41f00f4512292b8e449
Alejandro Redondo Villa <alexredondovilla@gmail.com>
2022-07-28 14:12:34 +0200

    pruebas sobre addCategory lanzando eventos usando jest functions

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -12,7 +12,9 @@ describe('Pruebas en <AddCategory />', () => {
   })
   test('Debe enviarse el valor del input al hacer submit', () => {
     const valueTest = 'Iron Man';
-    render(<AddCategory onAddCategory={() => {}}/>);
+    const onAddCategoryTest = jest.fn();
+    // render(<AddCategory onAddCategory={() => {}}/>);
+    render(<AddCategory onAddCategory={onAddCategoryTest}/>);
 
     const inputTest = screen.getByRole('textbox'); 
     const formTest = screen.getByRole('form'); // LO ENCUENTRA A TRAVES DE LA ETIQUETA aria-label 
@@ -21,6 +23,23 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.submit(formTest); // LIMPIA EL VALOR DEL value EN EL input
     screen.debug();
 
-    expect(inputTest.value).toBe('')
+    expect(inputTest.value).toBe('');
+    expect(onAddCategoryTest).toHaveBeenCalled();
+    // PRUEBA SI LA FUNCION FUE LLAMADA Y SI SE LE HA PASADO EL PARAMETRO INDICADO valueTest
+    expect(onAddCategoryTest).toHaveBeenCalledWith(valueTest);
+  })
+
+  test('No debe mandarse ningun valor si el input esta vacio', () => {
+    // SI EL INPUT TIENE UN VALOR CON LONGUITUD UNO O MENOR NUNCA SE LANZA EL submit
+
+    const onAddCategoryTest = jest.fn();
+    render(<AddCategory onAddCategory={onAddCategoryTest}/>);
+
+    const formTest = screen.getByRole('form'); // LO ENCUENTRA A TRAVES DE LA ETIQUETA aria-label 
+    fireEvent.submit(formTest); // LIMPIA EL VALOR DEL value EN EL input
+
+    // expect(onAddCategoryTest).toHaveBeenCalledTimes(0)
+    // CODIGO EQUIVALENTE
+    expect(onAddCategoryTest).not.toHaveBeenCalled();
   })
 })
